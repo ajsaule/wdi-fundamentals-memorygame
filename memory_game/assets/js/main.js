65b7998d79945f9e14f275ea179b4c692628fc47
ajsaule <andrejsaule8@gmail.com>
2020-03-09 12:09:12 +1100

    created game logic

---
+++ memory_game/assets/js/main.js
@@ -0,0 +1,9 @@
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped" + " " + cardTwo);
+console.log("User flipped" + " " + cardFour);
+
+console.log("Mi name chaiLATTE")
